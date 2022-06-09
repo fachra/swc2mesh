09786afcffe7aac431560ce5c12e388da8339289
Chengran <chengran.fang@inria.fr>
2022-06-09 15:50:54 +0200

    replace example.swc by real neuron

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
 from swc2mesh import Swc2mesh
 
-mesh = Swc2mesh('example.swc')
-mesh.build('example.ply', compartment='cell', simplification=True)
+mesh = Swc2mesh('04b_spindle3aFI.swc')
+mesh.build('04b_spindle3aFI.ply', compartment='cell', simplification=True)
