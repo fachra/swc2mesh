77647af567652aad249837df32a29f232c33ef0e
niccolozy <zy_graysky@hotmail.com>
2023-01-14 17:50:59 +0100

    Prevent recursive subprocess creation on Windows

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 from swc2mesh import Swc2mesh
 
-mesh = Swc2mesh('04b_spindle3aFI.swc')
-mesh.build('04b_spindle3aFI.ply', compartment='cell', simplification=True)
+if __name__ == '__main__':
+    mesh = Swc2mesh('04b_spindle3aFI.swc')
+    mesh.build('04b_spindle3aFI.ply', compartment='cell', simplification=True)
